ac8ed0ff3f23dbd3520b353f3e50a9dfcfe3f0a7
mmmiiiuuu <109843520+mmmiiiuuu@users.noreply.github.com>
2022-07-23 17:49:34 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,9 +18,9 @@ const DIM = 25;
 
 // Load images
 function preload() {
-  const path = "";
-  for (let i = 0; i < 13; i++) {
-    tileImages[i] = loadImage(`${path}/${i}/${i}.png`);
+  const path = "circuit1";
+  for (let i = 0; i < 25; i++) {
+    tileImages[i] = loadImage(`${path}/${i}.png`);
   }
 }
 
